4e368401758a11c18a6dc9284abc4bb582450586
Zoltan Hadur <zotya701@hotmail.hu>
2022-12-21 22:29:40 +0100

    Set focus to email when login page loads Execute login command when enter is pressed in email or password textbox on login page

--- ZoNo2/Views/LoginPage.xaml.cs
+++ ZoNo2/Views/LoginPage.xaml.cs
@@ -18,6 +18,16 @@ namespace ZoNo2.Views
     private async void Page_Loading(FrameworkElement sender, object args)
     {
       await ViewModel.Load();
+      Email.Focus(FocusState.Programmatic);
+      Email.SelectionStart = Email.Text.Length;
+    }
+
+    private void TextBox_KeyDown(object sender, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
+    {
+      if (e.Key == Windows.System.VirtualKey.Enter)
+      {
+        Login.Command.Execute(null);
+      }
     }
   }
 }
