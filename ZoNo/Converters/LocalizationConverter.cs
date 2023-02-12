5de31facfff830695c5e519ed0b9a90089f86dda
Zoltan Hadur <zotya701@hotmail.hu>
2023-02-12 23:40:21 +0100

    Move converters to app resource

---
+++ ZoNo/Converters/LocalizationConverter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZoNo.Helpers;
+
+namespace ZoNo.Converters
+{
+  public class LocalizationConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+      if (parameter is string formatString)
+      {
+        return string.Format(formatString, value).GetLocalized();
+      }
+      return value.ToString()?.GetLocalized() ?? DependencyProperty.UnsetValue;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
