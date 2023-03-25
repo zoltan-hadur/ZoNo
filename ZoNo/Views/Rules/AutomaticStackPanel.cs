d27d3432c3f5f42a6900725779e0c091fe04f299
Zoltan Hadur <zotya701@hotmail.hu>
2023-03-26 00:53:58 +0100

    AutomaticStackPanel for rules WIP

---
+++ ZoNo/Views/Rules/AutomaticStackPanel.cs
@@ -0,0 +1,197 @@
+﻿using CommunityToolkit.WinUI.UI;
+using CommunityToolkit.WinUI.UI.Animations;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Markup;
+using Microsoft.UI.Xaml.Media.Animation;
+using Microsoft.UI.Xaml.Shapes;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace ZoNo.Views.Rules
+{
+  [ContentProperty(Name = nameof(Children))]
+  public class AutomaticStackPanel : ContentControl
+  {
+    public static readonly DependencyProperty ChildrenProperty = DependencyProperty.Register(nameof(Children), typeof(ObservableCollection<object>), typeof(AutomaticStackPanel), new PropertyMetadata(null, OnChildrenChanged));
+    public static readonly DependencyProperty OrientationProperty = DependencyProperty.Register(nameof(Orientation), typeof(Orientation), typeof(AutomaticStackPanel), new PropertyMetadata(Orientation.Vertical, OnOrientationChanged));
+    public static readonly DependencyProperty StartingOrientationProperty = DependencyProperty.Register(nameof(StartingOrientation), typeof(Orientation), typeof(AutomaticStackPanel), new PropertyMetadata(Orientation.Vertical, OnStartingOrientationChanged));
+    public static readonly DependencyProperty ThresholdProperty = DependencyProperty.Register(nameof(Threshold), typeof(double), typeof(AutomaticStackPanel), null);
+    public static readonly DependencyProperty SpacingProperty = DependencyProperty.Register(nameof(Spacing), typeof(double), typeof(AutomaticStackPanel), new PropertyMetadata(null, OnSpacingChanged));
+
+    public ObservableCollection<object> Children
+    {
+      get => (ObservableCollection<object>)GetValue(ChildrenProperty);
+      set => SetValue(ChildrenProperty, value);
+    }
+
+    public Orientation Orientation
+    {
+      get => (Orientation)GetValue(OrientationProperty);
+      set => SetValue(OrientationProperty, value);
+    }
+
+    public Orientation StartingOrientation
+    {
+      get => (Orientation)GetValue(StartingOrientationProperty);
+      set => SetValue(StartingOrientationProperty, value);
+    }
+
+    public double Threshold
+    {
+      get => (double)GetValue(ThresholdProperty);
+      set => SetValue(ThresholdProperty, value);
+    }
+
+    public double Spacing
+    {
+      get => (double)GetValue(SpacingProperty);
+      set => SetValue(SpacingProperty, value);
+    }
+
+    private Grid _grid = new Grid();
+    private Canvas _canvas = new Canvas();
+
+    public AutomaticStackPanel()
+    {
+      VerticalContentAlignment = VerticalAlignment.Stretch;
+      HorizontalContentAlignment = HorizontalAlignment.Stretch;
+      Children = new ObservableCollection<object>();
+
+      var grid = new Grid();
+      grid.Children.Add(_grid);
+      grid.Children.Add(_canvas);
+      Content = grid;
+
+      SizeChanged += AutomaticStackPanel_SizeChanged;
+    }
+
+    private void AutomaticStackPanel_SizeChanged(object sender, SizeChangedEventArgs e)
+    {
+      var newValue = StartingOrientation == Orientation.Vertical ? e.NewSize.Width : e.NewSize.Height;
+      if (newValue >= Threshold)
+      {
+        Orientation = StartingOrientation == Orientation.Vertical ? Orientation.Horizontal : Orientation.Vertical;
+      }
+      else
+      {
+        Orientation = StartingOrientation;
+      }
+    }
+
+    public static void OnChildrenChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+    {
+      if (sender is AutomaticStackPanel panel)
+      {
+        if (e.OldValue is ObservableCollection<object> oldChildren)
+        {
+          oldChildren.CollectionChanged -= panel.Children_CollectionChanged;
+        }
+        if (e.NewValue is ObservableCollection<object> newChildren)
+        {
+          newChildren.CollectionChanged += panel.Children_CollectionChanged;
+        }
+      }
+    }
+
+    public static void OnOrientationChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+    {
+      if (sender is AutomaticStackPanel panel)
+      {
+        panel.RearrangeChildren();
+      }
+    }
+
+    public static void OnStartingOrientationChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+    {
+      if (sender is AutomaticStackPanel panel && e.NewValue is Orientation orientation)
+      {
+        panel.Orientation = orientation;
+      }
+    }
+
+    public static void OnSpacingChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+    {
+      if (sender is AutomaticStackPanel panel)
+      {
+        panel._grid.RowSpacing = panel._grid.ColumnSpacing = panel.Spacing;
+      }
+    }
+
+    private void Children_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+      switch (e.Action)
+      {
+        case NotifyCollectionChangedAction.Add:
+          {
+            foreach (UIElement item in e.NewItems!)
+            {
+              OnItemAdded(item);
+            }
+          }
+          break;
+      }
+    }
+
+    private void OnItemAdded(UIElement item)
+    {
+      var rectangle = new Rectangle();
+      _grid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+      _grid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+      _grid.Children.Add(rectangle);
+      _canvas.Children.Add(item);
+      rectangle.Loaded += (s, e) =>
+      {
+        item.SetValue(WidthProperty, rectangle.ActualWidth);
+        item.SetValue(HeightProperty, rectangle.ActualHeight);
+        item.GetVisual().Offset = rectangle.ActualOffset;
+        rectangle.Tag = true;
+      };
+      BindItems(rectangle, item);
+      RearrangeChildren();
+    }
+
+    private void RearrangeChildren()
+    {
+      for (int i = 0; i < _grid.Children.Count; ++i)
+      {
+        switch (Orientation)
+        {
+          case Orientation.Vertical:
+            _grid.Children[i].SetValue(Grid.RowProperty, i);
+            _grid.Children[i].SetValue(Grid.ColumnProperty, 0);
+            _grid.Children[i].SetValue(Grid.RowSpanProperty, 1);
+            _grid.Children[i].SetValue(Grid.ColumnSpanProperty, _grid.Children.Count);
+            break;
+          case Orientation.Horizontal:
+            _grid.Children[i].SetValue(Grid.RowProperty, 0);
+            _grid.Children[i].SetValue(Grid.ColumnProperty, i);
+            _grid.Children[i].SetValue(Grid.RowSpanProperty, _grid.Children.Count);
+            _grid.Children[i].SetValue(Grid.ColumnSpanProperty, 1);
+            break;
+        }
+      }
+    }
+
+    private void BindItems(FrameworkElement elementInGrid, UIElement elementInCanvas)
+    {
+      elementInGrid.SizeChanged += (s, e) =>
+      {
+        if (elementInGrid.Tag is bool isLoaded && isLoaded)
+        {
+          AnimationBuilder.Create()
+          .Size(
+            to: elementInGrid.ActualSize,
+            duration: TimeSpan.FromMilliseconds(600),
+            easingMode: EasingMode.EaseOut,
+            layer: FrameworkLayer.Xaml)
+          .Offset(
+            to: elementInGrid.ActualOffset,
+            easingMode: EasingMode.EaseOut,
+            duration: TimeSpan.FromMilliseconds(600))
+          .Start(elementInCanvas);
+        }
+      };
+    }
+  }
+}
--- ZoNo/Views/Rules/RulesPage.xaml.cs
+++ ZoNo/Views/Rules/RulesPage.xaml.cs
@@ -1,28 +1,4 @@
-using CommunityToolkit.WinUI.UI;
-using CommunityToolkit.WinUI.UI.Animations;
-using Microsoft.UI;
-using Microsoft.UI.Composition;
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Hosting;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Media.Animation;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Numerics;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Xml.Linq;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Foundation.Metadata;
-using Windows.UI;
-using ZoNo.ViewModels;
 using ZoNo.ViewModels.Rules;
 
 namespace ZoNo.Views.Rules
@@ -35,48 +11,6 @@ namespace ZoNo.Views.Rules
     {
       ViewModel = App.GetService<RulesViewModel>();
       InitializeComponent();
-
-      var time = 600;
-      importRulesPlaceholder.SizeChanged += (s, e) =>
-      {
-        AnimationBuilder.Create()
-          .Size(
-            to: new Vector2(importRulesPlaceholder.ActualSize.X, importRulesPlaceholder.ActualSize.Y),
-            duration: TimeSpan.FromMilliseconds(time),
-            easingMode: EasingMode.EaseOut,
-            layer: FrameworkLayer.Xaml)
-          .Offset(
-            to: new Vector2(importRulesPlaceholder.ActualOffset.X, importRulesPlaceholder.ActualOffset.Y),
-            easingMode: EasingMode.EaseOut,
-            duration: TimeSpan.FromMilliseconds(time))
-          .Start(importRules);
-      };
-      splitwiseRulesPlaceholder.SizeChanged += (s, e) =>
-      {
-        AnimationBuilder.Create()
-          .Size(
-            to: new Vector2(splitwiseRulesPlaceholder.ActualSize.X, splitwiseRulesPlaceholder.ActualSize.Y),
-            duration: TimeSpan.FromMilliseconds(time),
-            easingMode: EasingMode.EaseOut,
-            layer: FrameworkLayer.Xaml)
-          .Offset(
-            to: new Vector2(splitwiseRulesPlaceholder.ActualOffset.X, splitwiseRulesPlaceholder.ActualOffset.Y),
-            easingMode: EasingMode.EaseOut,
-            duration: TimeSpan.FromMilliseconds(time))
-          .Start(splitwiseRules);
-      };
-    }
-
-    private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
-    {
-      if (e.NewSize.Width > 1280)
-      {
-        VisualStateManager.GoToState(this, "WideState", false);
-      }
-      else
-      {
-        VisualStateManager.GoToState(this, "DefaultState", false);
-      }
     }
   }
 }
