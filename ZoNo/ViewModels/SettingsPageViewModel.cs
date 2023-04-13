41e158a7c727e455e2cb3e37d75d9fee5d873419
Zoltan Hadur <zotya701@hotmail.hu>
2023-04-13 22:24:06 +0200

    Import Export buttons for Rules in Settings

--- ZoNo/ViewModels/SettingsPageViewModel.cs
+++ ZoNo/ViewModels/SettingsPageViewModel.cs
@@ -2,16 +2,24 @@
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 using System.Reflection;
 using System.Windows.Input;
 using Windows.ApplicationModel;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Provider;
+using WinRT.Interop;
 using ZoNo.Contracts.Services;
 using ZoNo.Helpers;
+using ZoNo.Models;
 
 namespace ZoNo.ViewModels
 {
   public partial class SettingsPageViewModel : ObservableRecipient
   {
+    private readonly IRulesService _rulesService;
+    private readonly IDialogService _dialogService;
     private readonly IThemeSelectorService _themeSelectorService;
     private ElementTheme _elementTheme;
     private string _versionDescription;
@@ -30,8 +38,14 @@ namespace ZoNo.ViewModels
 
     public ICommand SwitchThemeCommand { get; }
 
-    public SettingsPageViewModel(IThemeSelectorService themeSelectorService, IMessenger messenger) : base(messenger)
+    public SettingsPageViewModel(
+      IRulesService rulesService,
+      IDialogService dialogService,
+      IThemeSelectorService themeSelectorService,
+      IMessenger messenger) : base(messenger)
     {
+      _rulesService = rulesService;
+      _dialogService = dialogService;
       _themeSelectorService = themeSelectorService;
       _elementTheme = _themeSelectorService.Theme;
       _versionDescription = GetVersionDescription();
@@ -61,6 +75,74 @@ namespace ZoNo.ViewModels
       }
 
       return $"{"AppDisplayName".GetLocalized()} - {version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+    }
+
+    [RelayCommand]
+    private async void ImportRules()
+    {
+      var openPicker = new FileOpenPicker();
+      var hWnd = App.MainWindow.GetWindowHandle();
+      InitializeWithWindow.Initialize(openPicker, hWnd);
+      openPicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+      openPicker.FileTypeFilter.Add(".json");
+
+      if (await openPicker.PickSingleFileAsync() is var file && file != null)
+      {
+        if (await FileIO.ReadTextAsync(file) is var json &&
+            json != null &&
+            await Json.ToObjectAsync<Dictionary<RuleType, IList<Rule>>>(json) is var rules &&
+            rules != null)
+        {
+          foreach (var rule in rules)
+          {
+            await _rulesService.SaveRulesAsync(rule.Key, rule.Value);
+          }
+          await ShowMessage("Import", "Success", file.Path);
+        }
+        else
+        {
+          await ShowMessage("Import", "Failure", file.Path);
+        }
+      }
+    }
+
+    [RelayCommand]
+    private async void ExportRules()
+    {
+      var savePicker = new FileSavePicker();
+      var hWnd = App.MainWindow.GetWindowHandle();
+      InitializeWithWindow.Initialize(savePicker, hWnd);
+      savePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+      savePicker.FileTypeChoices.Add("JSON", new List<string>() { ".json" });
+      savePicker.SuggestedFileName = "Rules.json";
+
+      if (await savePicker.PickSaveFileAsync() is var file && file != null)
+      {
+        CachedFileManager.DeferUpdates(file);
+        var rules = new Dictionary<RuleType, IList<Rule>>();
+        foreach (var ruleType in Enum.GetValues<RuleType>())
+        {
+          rules[ruleType] = await _rulesService.GetRulesAsync(ruleType);
+        }
+        var json = await Json.StringifyAsync(rules);
+        await FileIO.WriteTextAsync(file, json);
+        var status = await CachedFileManager.CompleteUpdatesAsync(file);
+        switch (status)
+        {
+          case FileUpdateStatus.Complete:
+          case FileUpdateStatus.CompleteAndRenamed:
+            await ShowMessage("Export", "Success", file.Path);
+            break;
+          default:
+            await ShowMessage("Export", "Failure", file.Path);
+            break;
+        }
+      }
+    }
+
+    private async Task ShowMessage(string importExport, string successFailure, string path)
+    {
+      await _dialogService.ShowDialogAsync(DialogType.Ok, "Settings_Dialog_Message".GetLocalized(), string.Format($"Settings_Dialog_{importExport}_{successFailure}".GetLocalized(), path));
     }
   }
 }
