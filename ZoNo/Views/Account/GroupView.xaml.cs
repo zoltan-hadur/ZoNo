3af979abe026c25d587d30189bc071be57418c3a
Zoltan Hadur <zotya701@hotmail.hu>
2023-07-19 16:32:37 +0200

    Refactor AccountPage, GroupView and UserView

--- ZoNo/Views/Account/GroupView.xaml.cs
+++ ZoNo/Views/Account/GroupView.xaml.cs
@@ -6,12 +6,26 @@ namespace ZoNo.Views.Account
 {
   public sealed partial class GroupView : UserControl
   {
-    public static readonly DependencyProperty GroupProperty = DependencyProperty.Register(nameof(Group), typeof(Group), typeof(GroupView), null);
+    public static readonly DependencyProperty PictureProperty = DependencyProperty.Register(nameof(Picture), typeof(string), typeof(GroupView), new PropertyMetadata("invalid"));
+    public static new readonly DependencyProperty NameProperty = DependencyProperty.Register(nameof(Name), typeof(string), typeof(GroupView), null);
+    public static readonly DependencyProperty MembersProperty = DependencyProperty.Register(nameof(Members), typeof(IEnumerable<User>), typeof(GroupView), null);
 
-    public Group Group
+    public string Picture
     {
-      get => (Group)GetValue(GroupProperty);
-      set => SetValue(GroupProperty, value);
+      get => (string)GetValue(PictureProperty);
+      set => SetValue(PictureProperty, value);
+    }
+
+    public new string Name
+    {
+      get => (string)GetValue(NameProperty);
+      set => SetValue(NameProperty, value);
+    }
+
+    public IEnumerable<User> Members
+    {
+      get => (IEnumerable<User>)GetValue(MembersProperty);
+      set => SetValue(MembersProperty, value);
     }
 
     public GroupView()
--- ZoNo/Views/Account/UserView.xaml.cs
+++ ZoNo/Views/Account/UserView.xaml.cs
@@ -1,17 +1,37 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Splitwise.Models;
 
 namespace ZoNo.Views.Account
 {
   public sealed partial class UserView : UserControl
   {
-    public static readonly DependencyProperty UserProperty = DependencyProperty.Register(nameof(User), typeof(User), typeof(UserView), null);
+    public static readonly DependencyProperty PictureProperty = DependencyProperty.Register(nameof(Picture), typeof(string), typeof(UserView), null);
+    public static readonly DependencyProperty FirstNameProperty = DependencyProperty.Register(nameof(FirstName), typeof(string), typeof(UserView), null);
+    public static readonly DependencyProperty LastNameProperty = DependencyProperty.Register(nameof(LastName), typeof(string), typeof(UserView), null);
+    public static readonly DependencyProperty EmailProperty = DependencyProperty.Register(nameof(Email), typeof(string), typeof(UserView), null);
 
-    public User User
+    public string Picture
     {
-      get => (User)GetValue(UserProperty);
-      set => SetValue(UserProperty, value);
+      get => (string)GetValue(PictureProperty);
+      set => SetValue(PictureProperty, value);
+    }
+
+    public string FirstName
+    {
+      get => (string)GetValue(FirstNameProperty);
+      set => SetValue(FirstNameProperty, value);
+    }
+
+    public string LastName
+    {
+      get => (string)GetValue(LastNameProperty);
+      set => SetValue(LastNameProperty, value);
+    }
+
+    public string Email
+    {
+      get => (string)GetValue(EmailProperty);
+      set => SetValue(EmailProperty, value);
     }
 
     public UserView()
