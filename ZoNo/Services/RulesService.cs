8faeb9f87344b1aec6d95b9668e6b1c66f1d1bed
Zoltan Hadur <zotya701@hotmail.hu>
2023-08-17 22:19:20 +0200

    Fix "The size of the state manager setting value has exceeded the limit" exception

--- ZoNo/Services/RulesService.cs
+++ ZoNo/Services/RulesService.cs
@@ -1,4 +1,5 @@
-﻿using ZoNo.Contracts.Services;
+﻿using Windows.Storage;
+using ZoNo.Contracts.Services;
 using ZoNo.Helpers;
 using ZoNo.Models;
 
@@ -6,15 +7,12 @@ namespace ZoNo.Services
 {
   public class RulesService : IRulesService
   {
-    private readonly ILocalSettingsService _localSettingsService;
-    private readonly string SettingName = "Rules";
-
     private Dictionary<RuleType, Rule[]> _rules = new Dictionary<RuleType, Rule[]>();
     private SemaphoreSlim _guard = new SemaphoreSlim(initialCount: 1, maxCount: 1);
 
-    public RulesService(ILocalSettingsService localSettingsService)
+    public RulesService()
     {
-      _localSettingsService = localSettingsService;
+
     }
 
     public async Task LoadRulesAsync()
@@ -22,7 +20,7 @@ namespace ZoNo.Services
       using var guard = await LockGuard.CreateAsync(_guard, TimeSpan.Zero);
       foreach (var type in Enum.GetValues<RuleType>())
       {
-        _rules[type] = await _localSettingsService.ReadSettingAsync<Rule[]>($"{SettingName}_{type}") ?? new Rule[] { };
+        _rules[type] = await ApplicationData.Current.LocalFolder.ReadAsync<Rule[]>($"Rules_{type}") ?? Array.Empty<Rule>();
       }
     }
 
@@ -36,7 +34,7 @@ namespace ZoNo.Services
     {
       using var guard = await LockGuard.CreateAsync(_guard, TimeSpan.FromSeconds(5));
       _rules[type] = rules.Select(rule => rule.Clone()).ToArray();
-      await _localSettingsService.SaveSettingAsync($"{SettingName}_{type}", _rules[type]);
+      await ApplicationData.Current.LocalFolder.SaveAsync($"Rules_{type}", _rules[type]);
     }
   }
 }
