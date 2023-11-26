343f000707bd15242d7e7b50641f38ed1117398f
Zoltan Hadur <zotya701@hotmail.hu>
2023-11-26 20:56:36 +0100

    Open expense editor when double clicking on expense

--- ZoNo/Views/ImportPage.xaml.cs
+++ ZoNo/Views/ImportPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
 using ZoNo.ViewModels;
 
 namespace ZoNo.Views
@@ -24,6 +25,14 @@ namespace ZoNo.Views
     {
       if (e.AddedItems.Count == 0) return;
       Expenses.ScrollIntoView(e.AddedItems[0]);
+    }
+
+    private void Expenses_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
+    {
+      if (e.OriginalSource is FrameworkElement element)
+      {
+        ViewModel.ExpensesViewModel.EditExpenseCommand?.Execute(element.DataContext);
+      }
     }
   }
 }
