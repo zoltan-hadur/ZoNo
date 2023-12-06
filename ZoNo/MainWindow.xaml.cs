8b104b19d9c20d3caaa6f4d7798f27ef6cdef4bb
Zoltan Hadur <zotya701@hotmail.hu>
2023-12-06 21:38:26 +0100

    Animate caption button colors upon activation deactivation

--- ZoNo/MainWindow.xaml.cs
+++ ZoNo/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Animation;
+using System.Diagnostics;
 using Windows.UI;
 
 namespace ZoNo
@@ -53,7 +54,7 @@ namespace ZoNo
       }
     }
 
-    private void MainWindow_Activated(object sender, WindowActivatedEventArgs args)
+    private async void MainWindow_Activated(object sender, WindowActivatedEventArgs args)
     {
       CommunityToolkit.WinUI.UI.Animations.AnimationBuilder
         .Create().Opacity(to: args.WindowActivationState == WindowActivationState.Deactivated ? 0.5 : 1)
@@ -74,6 +75,48 @@ namespace ZoNo
         TitleBarTextBlock.Foreground = (SolidColorBrush)TitleBarTextBlock.Resources[resourceKey];
       };
       storyboard.Begin();
+
+      try
+      {
+        if (args.WindowActivationState == WindowActivationState.Deactivated)
+        {
+          var duration = 250.0;
+          var stopwatch = Stopwatch.StartNew();
+          while (stopwatch.ElapsedMilliseconds < duration)
+          {
+            var from = NavigationFrame.ActualTheme == ElementTheme.Light ? Colors.Black : Colors.White;
+            var to = NavigationFrame.ActualTheme == ElementTheme.Light ? Colors.LightSlateGray : Colors.Gray;
+            var r = Convert.ToByte(double.Lerp(from.R, to.R, Math.Min(stopwatch.ElapsedMilliseconds / duration, 1.0)));
+            var g = Convert.ToByte(double.Lerp(from.G, to.G, Math.Min(stopwatch.ElapsedMilliseconds / duration, 1.0)));
+            var b = Convert.ToByte(double.Lerp(from.B, to.B, Math.Min(stopwatch.ElapsedMilliseconds / duration, 1.0)));
+            AppWindow.TitleBar.ButtonInactiveForegroundColor = Color.FromArgb(255, r, g, b);
+            await Task.Delay(1);
+          }
+          stopwatch.Stop();
+          AppWindow.TitleBar.ButtonInactiveForegroundColor = NavigationFrame.ActualTheme == ElementTheme.Light ? Colors.LightSlateGray : Colors.Gray;
+        }
+        else
+        {
+          var duration = 250.0;
+          var stopwatch = Stopwatch.StartNew();
+          while (stopwatch.ElapsedMilliseconds < duration)
+          {
+            var from = NavigationFrame.ActualTheme == ElementTheme.Light ? Colors.LightSlateGray : Colors.Gray;
+            var to = NavigationFrame.ActualTheme == ElementTheme.Light ? Colors.Black : Colors.White;
+            var r = Convert.ToByte(double.Lerp(from.R, to.R, Math.Min(stopwatch.ElapsedMilliseconds / duration, 1.0)));
+            var g = Convert.ToByte(double.Lerp(from.G, to.G, Math.Min(stopwatch.ElapsedMilliseconds / duration, 1.0)));
+            var b = Convert.ToByte(double.Lerp(from.B, to.B, Math.Min(stopwatch.ElapsedMilliseconds / duration, 1.0)));
+            AppWindow.TitleBar.ButtonForegroundColor = Color.FromArgb(255, r, g, b);
+            await Task.Delay(1);
+          }
+          stopwatch.Stop();
+          AppWindow.TitleBar.ButtonForegroundColor = NavigationFrame.ActualTheme == ElementTheme.Light ? Colors.Black : Colors.White;
+        }
+      }
+      catch
+      {
+
+      }
     }
   }
 }
