8bd5ef58e1210705d476366b8b888b727a819de3
Zoltan Hadur <zotya701@hotmail.hu>
2023-12-30 19:52:48 +0100

    Refactor Json helper class

--- ZoNo/Helpers/Json.cs
+++ ZoNo/Helpers/Json.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Text;
+using System.Text.Json;
 
 namespace ZoNo.Helpers
 {
@@ -6,18 +7,15 @@ namespace ZoNo.Helpers
   {
     public static async Task<T> ToObjectAsync<T>(string value)
     {
-      return await Task.Run(() =>
-      {
-        return JsonSerializer.Deserialize<T>(value);
-      });
+      using var stream = new MemoryStream(Encoding.UTF8.GetBytes(value));
+      return await JsonSerializer.DeserializeAsync<T>(stream);
     }
 
     public static async Task<string> StringifyAsync(object value)
     {
-      return await Task.Run(() =>
-      {
-        return JsonSerializer.Serialize(value);
-      });
+      using var stream = new MemoryStream();
+      await JsonSerializer.SerializeAsync(stream, value);
+      return Encoding.UTF8.GetString(stream.ToArray());
     }
   }
 }
