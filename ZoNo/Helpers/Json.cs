3c75f94316c7304b1ce1ab02167bf7b5bd50cff6
Zoltan Hadur <zotya701@hotmail.hu>
2024-01-18 20:07:49 +0100

    Add TupleJsonConverter

--- ZoNo/Helpers/Json.cs
+++ ZoNo/Helpers/Json.cs
@@ -3,18 +3,23 @@ using System.Text.Json;
 
 namespace ZoNo.Helpers
 {
-  public static class Json
+  public static partial class Json
   {
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new()
+    {
+      Converters = { new TupleJsonConverter() }
+    };
+
     public static async Task<T> ToObjectAsync<T>(string value)
     {
       using var stream = new MemoryStream(Encoding.UTF8.GetBytes(value));
-      return await JsonSerializer.DeserializeAsync<T>(stream);
+      return await JsonSerializer.DeserializeAsync<T>(stream, _jsonSerializerOptions);
     }
 
     public static async Task<string> StringifyAsync(object value)
     {
       using var stream = new MemoryStream();
-      await JsonSerializer.SerializeAsync(stream, value);
+      await JsonSerializer.SerializeAsync(stream, value, _jsonSerializerOptions);
       return Encoding.UTF8.GetString(stream.ToArray());
     }
   }
