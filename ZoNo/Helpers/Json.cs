eb1de9130de658b3d8fd980d13dd1da3d8642408
Zoltan Hadur <zotya701@hotmail.hu>
2024-02-04 21:17:33 +0100

    Remove trace from Json helper Add JsonStringEnumConverter to json serializer options in Json helper

--- ZoNo/Helpers/Json.cs
+++ ZoNo/Helpers/Json.cs
@@ -1,30 +1,30 @@
 ﻿using System.Text;
 using System.Text.Json;
-using Tracer.Contracts;
+using System.Text.Json.Serialization;
 
 namespace ZoNo.Helpers
 {
   public static class Json
   {
-    private static readonly ITraceFactory _traceFactory = App.GetService<ITraceFactory>();
-    private static readonly JsonSerializerOptions _jsonSerializerOptions = new() { Converters = { new TupleJsonConverter() } };
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new()
+    {
+      Converters = {
+        new TupleJsonConverter(),
+        new JsonStringEnumConverter()
+      }
+    };
 
     public static async Task<T> ToObjectAsync<T>(string value)
     {
-      using var trace = _traceFactory.CreateNew();
-      trace.Debug(Format([GetTypeName(typeof(T)), value]));
       using var stream = new MemoryStream(Encoding.UTF8.GetBytes(value));
       return await JsonSerializer.DeserializeAsync<T>(stream, _jsonSerializerOptions);
     }
 
     public static async Task<string> StringifyAsync(object value)
     {
-      using var trace = _traceFactory.CreateNew();
       using var stream = new MemoryStream();
       await JsonSerializer.SerializeAsync(stream, value, _jsonSerializerOptions);
-      var result = Encoding.UTF8.GetString(stream.ToArray());
-      trace.Debug(Format([GetTypeName(value.GetType()), result]));
-      return result;
+      return Encoding.UTF8.GetString(stream.ToArray());
     }
   }
 }
